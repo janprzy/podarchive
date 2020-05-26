00d50bec3514dc4bb898fa900a47586409ac6943
Jan Przybylak <jp@j-p.space>
2020-05-26 19:33:29 +0200

    Adjusted file names

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # This command compiles this program into an executable binary file.
+# The binary file will be named "podarchive-cli". For some reason it needs to have the same name as the Perl file for this to work.
 
 # pp fails to detect and include some necessary modules, these need to be manually added via the '-M' flags
 
@@ -8,8 +9,8 @@
 # [ ] Linux
 # [ ] FreeBSD
 
-pp -v cli.pl podarchivelib.pl \
-   -o cli \
+pp -v podarchive-cli.pl podarchivelib.pl \
+   -o podarchive-cli \
  -M XML::RSS::Parser::Element\
  -M XML::RSS::Parser::Feed\
  -M XML::RSS::Parser::Characters\
